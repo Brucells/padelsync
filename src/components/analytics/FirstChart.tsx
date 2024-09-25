63d74f42ed3011f7d9763fe0ffa3f6231c2495de
Brucells <brucells@hotmail.com>
2024-09-25 18:00:46 +0200

    Create analytics page

---
+++ src/components/analytics/FirstChart.tsx
@@ -0,0 +1,70 @@
+import { Crosshair } from "lucide-react"
+import { PolarAngleAxis, PolarGrid, Radar, RadarChart } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A radar chart with a grid filled"
+
+const chartData = [
+    { shot: "Service", desktop: 89 },
+    { shot: "Smash", desktop: 24 },
+    { shot: "Volley", desktop: 48 },
+    { shot: "Lob", desktop: 66 },
+    { shot: "Revers", desktop: 72 },
+    { shot: "Droit", desktop: 81 },
+]
+
+const chartConfig = {
+    desktop: {
+        label: "%",
+        color: "hsl(var(--primary))",
+    },
+} satisfies ChartConfig
+
+export function FirstChart() {
+    return (
+        <Card className="bg-gradient-to-b from-accent mb-2">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Précision</CardTitle>
+                <Crosshair strokeWidth={1.25} className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+
+            <CardContent className="pb-0">
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[300px]"
+                >
+                    <RadarChart data={chartData}>
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <PolarGrid className="fill-[--color-desktop] opacity-5" />
+                        <PolarAngleAxis dataKey="shot" />
+                        <Radar
+                            dataKey="desktop"
+                            fill="var(--color-desktop)"
+                            fillOpacity={0.7}
+                        />
+                    </RadarChart>
+                </ChartContainer>
+            </CardContent>
+            <CardDescription className="flex justify-center mb-2">
+                Précision de vos coups en pourcentage de réussite
+            </CardDescription>
+
+        </Card>
+    )
+}
--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,7 +1,14 @@
+import { FirstChart } from "@/components/analytics/FirstChart";
+import { ChartNetwork } from "lucide-react";
+
 function Analytics() {
     return ( 
         <>
-            <h1>Analytics</h1>
+            <div className="flex flex-row items-center mb-2">
+                <ChartNetwork strokeWidth={2} className="ml-2 mr-1 h-5" />
+                <h1 className="text-lg font-semibold md:text-2xl">Analytics</h1>
+            </div>
+            <FirstChart />
         </>
      );
 }
