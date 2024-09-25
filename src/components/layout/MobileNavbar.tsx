1af1f55556dfd2fc9d87f662ec854272c3424fd9
Brucells <brucells@hotmail.com>
2024-09-25 14:02:46 +0200

    Test some ui

--- src/components/layout/MobileNavbar.tsx
+++ src/components/layout/MobileNavbar.tsx
@@ -1,8 +1,55 @@
+import { Cctv, ChartArea, ChartBar, Diameter, Dribbble, Home, LayoutDashboard } from "lucide-react";
+
 function MobileNavbar() {
     return (
         <div className="w-full">
-            <div className="block bg-pink-100 py-5 md:hidden">
-                <h1 className="text-center text-xl">Mobile navigation</h1>
+            <div className="block py-1 md:hidden">
+                <div className="grid place-content-center grid-cols-5">
+                    <div>
+                        <a
+                            href="#"
+                            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-primary-foreground mx-auto"
+                        >
+                            <Home className="h-4 w-4 transition-all group-hover:scale-110" />
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="#"
+                            className="mx-auto flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                        >
+                            <Dribbble className="h-5 w-5" />
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="#"
+                            className="mx-auto flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                        >
+                            <ChartArea className="h-5 w-5" />
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="#"
+                            className="mx-auto flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                        >
+                            <ChartBar className="h-5 w-5" />
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="#"
+                            className="mx-auto flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                        >
+                            <Cctv className="h-5 w-5" />
+                        </a>
+                    </div>
+
+
+
+
+                </div>
             </div>
         </div>
     );
